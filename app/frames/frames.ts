22f97ccd9b380cfb82a6cfb8c6ee7b7748fb9ebc
Max Alekseenko <nnwen0609@gmail.com>
2024-06-06 00:09:23 +0200

    change base path

--- app/frames/frames.ts
+++ app/frames/frames.ts
@@ -2,7 +2,6 @@ import { farcasterHubContext } from 'frames.js/middleware';
 import { createFrames } from 'frames.js/next';
 
 export const frames = createFrames({
-  basePath: '/frames',
   middleware: [
     farcasterHubContext({
       // remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
