3687c3340ef49373e37c18c94f58dd3d26341de7
Max Alekseenko <nnwen0609@gmail.com>
2024-06-06 15:34:48 +0200

    replace eth with base

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000';
 
-export const BLOCKSCOUT_API_URL = 'https://eth.blockscout.com/api/v2';
-export const BLOCKSCOUT_URL = 'https://eth.blockscout.com';
+export const BLOCKSCOUT_API_URL = 'https://base.blockscout.com/api/v2';
+export const BLOCKSCOUT_URL = 'https://base.blockscout.com';
